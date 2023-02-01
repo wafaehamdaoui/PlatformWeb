b8951c3db7b26e0d92cca3c60f670c21dd8bda34
wafaehamdaoui <hamdaouiwafae2000@gmail.com>
2023-02-01 01:02:51 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const oracledb = require('oracledb')
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+app.get('/resources',(req,res)=>{
+    async function fetchDataResources(){
+        try{
+            const connection = await oracledb.getConnection({
+                user:'USER_ADMIN',
+                password:'123456',
+                connectionString:'localhost/orcl'
+            });
+            const result = await connection.execute('SELECT * FROM USER_ADMIN.Ressource ');
+            return result.rows;
+        } catch (error){
+            return error;
+
+        }
+    }
+    fetchDataResources().then(dbRes =>{
+        console.log('hi',dbRes)
+        res.send(dbRes);
+    }).catch(err=>{
+        res.send(err)
+    })
+})
+app.listen(5000, () => {
+    console.log('Server Listen to port 5000');
+})

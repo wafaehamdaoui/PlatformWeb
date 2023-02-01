1e40bfe7a3bb61a581cded3bb92273356a2b2e63
wafaehamdaoui <hamdaouiwafae2000@gmail.com>
2023-02-01 18:45:22 +0100

    cimit 4

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,8 @@ app.get('/resources',(req,res)=>{
     async function fetchDataResources(){
         try{
             const connection = await oracledb.getConnection({
-                user:'USER_ADMIN',
-                password:'123456',
+                user:'USER_Student',
+                password:'654321',
                 connectionString:'localhost/orcl'
             });
             const result = await connection.execute('SELECT * FROM USER_ADMIN.Ressource ');
@@ -27,6 +27,32 @@ app.get('/resources',(req,res)=>{
     }).catch(err=>{
         res.send(err)
     })
+})
+app.get('/users',(req,res)=>{
+    async function fetchDatausers(){
+        try{
+            const connection = await oracledb.getConnection({
+                user:'user_admin',
+                password:'123456',
+                connectionString:'localhost/orcl'
+            });
+            const result = await connection.execute("SELECT user_id,username,created FROM ALL_USERS where EXTRACT( YEAR FROM created)=2023");
+            return result.rows;
+        } catch (error){
+            return error;
+
+        }
+    }
+    fetchDatausers().then(dbRes =>{
+        console.log('hi',dbRes)
+        res.send(dbRes)
+    }).catch(err=>{
+        res.send(err)
+    })
+})
+
+app.post('/login',(req,res)=>{
+    async
 })
 app.listen(5000, () => {
     console.log('Server Listen to port 5000');
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+ 
 import RessourceList from './RessourceList';
-function App() {
+import UserList from './UserList';
+import Login from './login';
+// We use Route in order to define the different routes of our application
+const App = () => {
   return (
-    <div className="App">
-      <RessourceList />
+    <div>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/resources" element={<RessourceList />} />
+        <Route exact path="/users" element={<UserList />} />
+      </Routes>
     </div>
   );
-}
-
-export default App;
+ };
+  
+ export default App;
